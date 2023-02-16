36bffdefb0905e44fcd99ebe5fc79593a3eb4c51
kagchi <yonuxbot@gmail.com>
2023-02-16 19:22:23 +0700

    feat: add send function helper to command context

--- src/Lib/CommandContext.ts
+++ src/Lib/CommandContext.ts
@@ -1,5 +1,6 @@
 import { BaseInteraction, Message } from "@nezuchan/core";
 import { ArgumentStream } from "@sapphire/lexure";
+import { APIInteractionResponseCallbackData, RESTPostAPIChannelMessageJSONBody } from "discord-api-types/v10";
 
 export class CommandContext {
     public constructor(private readonly context: BaseInteraction | Message, public messageArgs: ArgumentStream) { }
@@ -12,11 +13,28 @@ export class CommandContext {
         return this.context as BaseInteraction;
     }
 
-    public get isInteraction(): boolean {
+    public isInteraction(): boolean {
         return this.context instanceof BaseInteraction;
     }
 
-    public get isMessage(): boolean {
+    public isMessage(): boolean {
         return this.context instanceof Message;
+    }
+
+    public send(options: APIInteractionResponseCallbackData | RESTPostAPIChannelMessageJSONBody): Promise<BaseInteraction> | Promise<Message> {
+        if (this.isInteraction()) {
+            if (this.interaction.isCommandInteraction() && (this.interaction.isContextMenuInteraction() || this.interaction.isCommandInteraction())) {
+                if (this.interaction.deferred && !this.interaction.replied) {
+                    return this.interaction.editReply(options);
+                }
+
+                if (this.interaction.replied) {
+                    return this.interaction.followUp(options);
+                }
+            }
+
+            return this.interaction.reply(options);
+        }
+        return this.message.client.sendMessage(options, this.message.channelId!);
     }
 }
