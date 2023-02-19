b33b620d47d2ef4fca52380b0a8277306c81927c
kagchi <yonuxbot@gmail.com>
2023-02-19 19:32:58 +0700

    fix: add missing func

--- src/Lib/Preconditions/Conditions/PreconditionConditionOr.ts
+++ src/Lib/Preconditions/Conditions/PreconditionConditionOr.ts
@@ -86,5 +86,26 @@ export const PreconditionConditionOr: IPreconditionCondition = {
         }
 
         return error ?? Result.ok();
+    },
+    async interactionHandlerSequential(interaction, handler, entries, context) {
+        let error: PreconditionContainerResult | null = null;
+        for (const child of entries) {
+            const result = await child.interactionHandlerRun(interaction, handler, context);
+            if (result.isOk()) return result;
+            error = result;
+        }
+
+        return error ?? Result.ok();
+    },
+    async interactionHandlerParallel(interaction, handler, entries, context) {
+        const results = await Promise.all(entries.map(entry => entry.interactionHandlerRun(interaction, handler, context)));
+
+        let error: PreconditionContainerResult | null = null;
+        for (const result of results) {
+            if (result.isOk()) return result;
+            error = result;
+        }
+
+        return error ?? Result.ok();
     }
 };
